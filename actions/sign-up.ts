0cc98d64c4b7b534654f818f4ce39c49ba183776
Kent0710 <christiankent001@gmail.com>
2024-06-02 08:18:40 +0800

    capital and space allowed in sign up

--- actions/sign-up.ts
+++ actions/sign-up.ts
@@ -16,7 +16,7 @@ export default async function signup(_: any, formData: FormData): Promise<Action
       typeof username !== "string" ||
       username.length < 3 ||
       username.length > 31 ||
-      !/^[a-z0-9_-]+$/.test(username)
+      !/^[a-zA-Z0-9 _-]+$/.test(username)
     ) {
       return {
         error: "Invalid username",
